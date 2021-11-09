cd87799580315afc24a34a7320ecd1ed3a91567f
Sabari <r.sabarinathan22@gmail.com>
2021-11-09 22:51:21 +0530

    cards-can-match

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -31,9 +31,30 @@ function App() {
 
 	//handle a choice
 	const handleChoice = (card) => {
-
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("CArds are match");
+        resetTurn();
+      } else {
+        console.log("Cards doesnot match");
+        resetTurn();
+      }
+    }
+
+  },[choiceOne,choiceTwo])
+
+  //reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns+1)
+  }
 
 	return (
 		<div className='App'>
