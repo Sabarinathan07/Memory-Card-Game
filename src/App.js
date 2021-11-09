94148d73102ceb4da6e669d7bf1ea31f1ded1307
Sabari <r.sabarinathan22@gmail.com>
2021-11-09 22:33:39 +0530

    set-choice-added

--- src/App.js
+++ src/App.js
@@ -3,46 +3,50 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
-]
+	{ src: '/img/helmet-1.png' },
+	{ src: '/img/potion-1.png' },
+	{ src: '/img/ring-1.png' },
+	{ src: '/img/scroll-1.png' },
+	{ src: '/img/shield-1.png' },
+	{ src: '/img/sword-1.png' },
+];
 
 function App() {
+	const [cards, setCards] = useState([]);
+	const [turns, setTurns] = useState(0);
 
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
+	const [choiceOne, setChoiceOne] = useState(null);
+	const [choiceTwo, setChoiceTwo] = useState(null);
 
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  
+	//Shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+		setCards(shuffledCards);
+		setTurns(0);
+	};
 
-  //Shuffle cards 
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }))
-    setCards(shuffledCards);
-    setTurns(0);
-  }
+	// console.log(cards, turns);
 
-  console.log(cards, turns);
+	//handle a choice
+	const handleChoice = (card) => {
 
-  return (
-    <div className="App">
-      <h1>Magic Match</h1>
-      <button onClick={shuffleCards}>New Game</button>
-      
-      <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard key={card.id} card={card} />
-        ))}
-      </div>
-    </div>
-  );
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+	return (
+		<div className='App'>
+			<h1>Magic Match</h1>
+			<button onClick={shuffleCards}>New Game</button>
+
+			<div className='card-grid'>
+				{cards.map((card) => (
+					<SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
